eff82af1c05519e467921e01482ae8a035bc99ee
Enrique Pittaluga <ep7m@me.com>
2020-04-06 14:35:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,9 +21,13 @@ def loop_message_n_times(message, int)
 end
     
     
-def output_array()
-  
-  
+def output_array(array)
+  index = 0
+  while (index < array.length) do 
+    puts array[index]
+    index += 1
+  end
+
 end
   
 def return_string_array()
