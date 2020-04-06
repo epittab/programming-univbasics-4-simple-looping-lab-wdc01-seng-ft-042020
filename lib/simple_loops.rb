794fed8e013aefd99eb4da78ab25135f85016580
Enrique Pittaluga <ep7m@me.com>
2020-04-06 14:32:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,12 @@
 # Write your methods here
 
-def loop_message_five_times()
+def loop_message_five_times(message)
+  count = 5
   
+  while (count > 0) do
+    puts message
+    count -= 1
+  end
   
 end
   
